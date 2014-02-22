e499ab6f6e56466f40059c515778433131dfdfeb
jonask1337 <sieg.tronje@gmail.com>
2014-02-22 13:12:15 +0100

    Jonas Änderung

---
+++ pÄnderung/uml-editor-neu/src/fk/Attribut.java
@@ -0,0 +1,83 @@
+package fk;
+
+/**
+ * Objekte der Klasse Attribut repr�sentieren Attribute
+ * <br/>
+ * Attribute haben einen Namen "name" und
+ * einen String "datentyp" = {int, double, long, float, String, char} 
+ * 
+ * @author Gruppe1
+ * 
+ */
+public class Attribut {
+
+	/**
+	 * name, beinhaltet den Namen des Attributs
+	 * 
+	 */
+	private String name;
+	
+	
+	/**
+	 * datentyp, ist das Attributentyp
+	 * 
+	 * @pre datentyp = {int, double, long, float, String, char}
+	 * @post Vorbedingung erf�llt!
+	 * 
+	 */
+	private String datentyp;
+
+	/**
+	 * Konstruktor
+	 * 
+	 * @param name Namen des Attributs
+	 * @param datentyp Typ des Attributs 
+	 */
+	public Attribut(String name, String datentyp) {
+		this.name = name;
+		this.datentyp = datentyp;
+	}
+
+	/**
+	 * Liefert den Namen des Attributs
+	 * 
+	 * @pre !null
+	 * @return String name, Namen des Attributs
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Ver�ndert oder weist dem Attribut einen neuen name zu.
+	 * 
+	 * @param name Name des Attributs
+	 * @post name ist der gew�nschte Attributname
+	 * @modifies this
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Liefert den datentyp des Attributs 
+	 * 
+	 * @pre !null
+	 * @return String datentyp, Typ des Attributs
+	 */
+	public String getDatentyp() {
+		return datentyp;
+	}
+
+	/**
+	 * Ver�ndert oder weist dem Attribut einen neuen Datentyp zu
+	 * 
+	 * @param datentyp Typ des Attributs
+	 * @post datentyp ist der gew�nschte Attributtyp
+	 * @modifies this
+	 */
+	public void setDatentyp(String datentyp) {
+		this.datentyp = datentyp;
+	}
+
+}
---
+++ pÄnderung/uml-editor-neu/src/gui/KlasseEditfenster.java
@@ -0,0 +1,118 @@
+package gui;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+import fk.Attribut;
+import fk.MyTableModel;
+
+
+public class KlasseEditfenster extends JFrame{
+	
+	JPanel jp;
+	JPanel northJP;
+	JPanel centerJP;
+	JButton jb1;
+	JButton jb2;
+	private JTable attributTable;
+	public JTable getAttributTable() {
+		return attributTable;
+	}
+
+	JTextField atrN;
+	JTextField klassenName;
+	
+	JComboBox<String> datentypen;
+
+	public JPanel getJp() {
+		return jp;
+	}
+
+	public void init() {
+		
+		jp = new JPanel();
+		northJP = new JPanel();
+		centerJP = new JPanel();
+		jp.setLayout(new BorderLayout(5,5));
+		northJP.setLayout(new GridLayout(0,3,4,2));
+		centerJP.setLayout(new GridLayout(0,3,4,2));
+		JLabel jlA = new JLabel("Atrname:");
+		JLabel jlK = new JLabel("Klassenname:");		
+		klassenName = new JTextField();			
+		atrN = new JTextField();		
+		String [] dt = {"int","String"};
+		datentypen = new JComboBox<>(dt);		
+		final ArrayList<ArrayList<String>> data = new ArrayList<>();
+		ArrayList<String> row = new ArrayList<String>();
+		jb1 = new JButton("Atr hinz");
+		jb2 = new JButton("Atr löschen");
+		attributTable = new JTable(new MyTableModel(data));
+		//Atribut button
+				jb1.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent arg0) {
+						//kled.setVisible(false);
+						ArrayList<String> row = new ArrayList<String>();					
+						row.add(atrN.getText());
+						row.add((String)datentypen.getSelectedItem());
+						data.add(row);
+						TableModel dm = attributTable.getModel();
+						((AbstractTableModel) dm).fireTableDataChanged(); 						
+					}
+				});
+		northJP.add(jlK);
+		northJP.add(klassenName);
+		northJP.add(new JLabel(""));
+		northJP.add(jlA);
+		northJP.add(atrN);
+		northJP.add(jb1);
+		northJP.add(new JLabel("Datentyp:"));
+		northJP.add(datentypen);		
+		northJP.add(jb2);
+		
+		jp.add(northJP,BorderLayout.PAGE_START);		
+		jp.add(attributTable, BorderLayout.CENTER);	
+		JScrollPane scrollPane = new JScrollPane(attributTable);
+		jp.add(scrollPane);
+		this.add(jp);
+
+		this.setPreferredSize(new Dimension(400, 300));
+		this.pack();
+		this.setVisible(true);
+	}
+
+	public JTextField getAtrN() {
+		return atrN;
+	}
+
+	public JTextField getKlassenName() {
+		return klassenName;
+	}
+
+	public JButton getJb1() {
+		return jb1;
+	}
+
+}
